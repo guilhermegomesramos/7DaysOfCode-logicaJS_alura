3fbd442f0c9b6ee0976ba440c56676caef45bbb6
guilhermegomesramos <168496717+guilhermegomesramos@users.noreply.github.com>
2024-07-14 15:18:13 -0300

    dia 3 finalizado

---
+++ dia2/dia2.js
@@ -0,0 +1,9 @@
+
+const Nome = prompt ('Qual o seu nome?')
+const Idade = prompt ('Quantos anos você tem?')
+const LinguagemDeProgramacao = prompt ('Qual linguagem de progamação você está estudando?')
+const Mensagem = `Olá ${Nome} você tem ${Idade}
+anos e já está aprendendo ${LinguagemDeProgramacao} !`
+
+alert (Mensagem)
+
