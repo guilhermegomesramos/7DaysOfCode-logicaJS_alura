d22eb9f97e975016959913c90834fb57bb7d5bb0
guilhermegomesramos <168496717+guilhermegomesramos@users.noreply.github.com>
2024-07-15 19:55:58 -0300

    1.2

--- dia2/dia2.js
+++ dia2/dia2.js
@@ -1,9 +1,18 @@
 
-const Nome = prompt ('Qual o seu nome?')
-const Idade = prompt ('Quantos anos você tem?')
-const LinguagemDeProgramacao = prompt ('Qual linguagem de progamação você está estudando?')
+const Nome = prompt ('Qual o seu nome?');
+const Idade = prompt ('Quantos anos você tem?');
+const LinguagemDeProgramacao = prompt ('Qual linguagem de progamação você está estudando?');
 const Mensagem = `Olá ${Nome} você tem ${Idade}
-anos e já está aprendendo ${LinguagemDeProgramacao} !`
+anos e já está aprendendo ${LinguagemDeProgramacao} !`;
 
-alert (Mensagem)
+alert (Mensagem);
+
+const gosta = prompt (`Você gosta de estudar ${LinguagemDeProgramacao} ? Respode com o número 1 - SIM ou 2 - NÃO`);
+if (gosta == 1) {
+    alert('Muito');
+} 
+if (gosta == 2) {
+    alert('Ahh');
+}
+
 
